63ad84e038881fca8972ecb62aeb7b959294638f
AlekseiAnikeev <info.anikeev@gmail.com>
2022-12-30 00:55:46 +0300

    bug fix

--- src/main/java/ru/agentche/model/Person.java
+++ src/main/java/ru/agentche/model/Person.java
@@ -9,6 +9,9 @@ public class Person {
     private int id;
     private String name;
 
+    public Person() {
+    }
+
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
